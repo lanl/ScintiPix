ea24218cff387a03a9bcfaf3baa285d91fbec9d1
Alexander M. Long <alexlong@lanl.gov>
2026-06-05 15:41:49 -0600

    Add unit test for primary schema constants including source time

--- test/unit/src/common/test_hdf5_schema.py
+++ test/unit/src/common/test_hdf5_schema.py
@@ -64,6 +64,30 @@ class Hdf5SchemaTests(unittest.TestCase):
             ),
         )
 
+    def test_primary_schema_constants_include_source_time(self) -> None:
+        from src.common.hdf5_schema import DATASET_PRIMARIES
+        from src.common.hdf5_schema import PRIMARY_FIELDS
+        from src.common.hdf5_schema import SOURCE_TIME_FIELD
+
+        self.assertEqual(DATASET_PRIMARIES, "primaries")
+        self.assertEqual(SOURCE_TIME_FIELD, "source_time_ns")
+        self.assertEqual(
+            PRIMARY_FIELDS,
+            (
+                "gun_call_id",
+                "primary_track_id",
+                "primary_species",
+                "primary_x_mm",
+                "primary_y_mm",
+                "primary_energy_MeV",
+                "source_time_ns",
+                "primary_interaction_time_ns",
+                "primary_created_secondary_count",
+                "primary_generated_optical_photon_count",
+                "primary_detected_optical_interface_photon_count",
+            ),
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
