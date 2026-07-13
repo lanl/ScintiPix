55ad8bac16f2e8e3fef5dcb71b0be32bdb2c560d
Alexander M. Long <alexlong@lanl.gov>
2026-07-13 15:46:23 -0600

    Update default YAML path in run_autofocus_with_transport.py to EJ200_siemens_50mm.yaml

--- examples/autofocus/run_autofocus_with_transport.py
+++ examples/autofocus/run_autofocus_with_transport.py
@@ -24,7 +24,7 @@ from examples.analysis.imaging.plotter import (  # noqa: E402
 )
 
 DEFAULT_YAML_PATH = (
-    REPO_ROOT / "examples" / "yamlFiles" / "autofocus_siemens_star_50mm.yaml"
+    REPO_ROOT / "examples" / "yamlFiles" / "EJ200_siemens_50mm.yaml"
 )
 
 
